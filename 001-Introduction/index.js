a6693d3b7a9a48c750cf299215f14609ce899e09
akhlasadnan <adnanakhlas39@gmail.com>
2023-08-30 00:02:46 +0600

    Initial Commit

---
+++ 001-Introduction/index.js
@@ -0,0 +1,10 @@
+console.log('Hello World');
+console.log('I am Adnan Akhlas');
+console.log('I\'m adnan')
+console.log('My age is ' + 20)
+console.log('I love my country');
+console.log(2);
+console.log(1.254);
+console.log(2+2);
+console.log('2 + ' + 2)
+
---
+++ 002-Variables/index.js
@@ -0,0 +1,29 @@
+var naam = 'Sabbir Rahman'
+var age = 24
+
+var num1 = 4
+var num2 = 8
+
+
+
+var total = num1 + num2 
+
+var multiply = num1 * num2
+
+var divide = num2 / num1
+
+var subs = num2 - num1
+
+console.log('My name is ' + naam );
+console.log('My age is ' + age);
+console.log('I am ' + naam + ' My age is ' + age)
+
+
+console.log('Total is: ' + total)
+console.log('Final Ouput is:' + multiply)
+console.log('Final Ouput is:' + divide)
+console.log('Final Output is: ' + subs)
+
+console.log(total + multiply + divide + subs)
+
+console.log("Addition: " + total  + "Multiplication:" + multiply  + "Division: " + divide +  " Substraction: " + subs)
