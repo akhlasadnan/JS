c2654e5749c03f50a2dd99a184a1cc0c9c67accf
akhlasadnan <adnanakhlas39@gmail.com>
2023-09-07 00:00:50 +0600

    Data Types

---
+++ 005-Data Types/data-types.js
@@ -0,0 +1,59 @@
+// let x = "John Doe";
+// let y = 5.4;
+
+// let a = 4;
+// let b = 8;
+// let c = a > b;
+// console.log(c);
+
+// const myObject = {name:"adnan", age:20, location:"dhaka"};
+// const myArray = ["Istiake", "Sujon", "Mahim"];
+// const date = new date("2023-09-06");
+
+
+
+// let y; // y is undefined
+// y = "Shuvo" // y is string
+// let x = "shuvo";
+// let z = 8;
+// let c = 1;
+
+// let p = BigInt("123456789012345678901234567890");
+// let b = x == y;
+
+// console.log(b)
+
+
+
+
+
+// let length = 16; 
+// let firstName = "David Johnson";
+
+// const person = {
+//     firstName: "David",
+//     lastName: "Jhonson"
+// };
+
+
+
+// function myFunction() {
+//     let x = 10;
+//     let y = 5;
+//     let z = x * y;
+//     return z;
+// }
+// myFunction()
+
+
+// const person = {
+//     firstName: "David",
+//     lastName: "Jhonson",
+//     age: 20,
+//     fullName: function() {
+//         return person.firstName + person.lastName;
+//     },    
+// }
+// console.log(person.fullName())
+
+
